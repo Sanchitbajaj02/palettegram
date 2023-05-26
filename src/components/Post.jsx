1da395f046540becf8334c41e6b798fa4cef9b70
kunal kukreja <111633675+kunal091001@users.noreply.github.com>
2023-05-27 00:44:32 +0530

    trendingposts and feed updated (#13)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Post = ({ index }) => {
   return (
-    <Link className="w-[80%] h-80 border" to={`/post/${index}`}>
+    <Link className="w-[100%] h-80 border" to={`/post/${index}`}>
       <div>Post {index}</div>
     </Link>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./Styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
