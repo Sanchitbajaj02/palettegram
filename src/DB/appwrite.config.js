c772b9c95c4d824ad446605b27015b0c857e0583
Sanchit Bajaj <sanchit02@outlook.com>
2023-06-01 00:26:05 +0530

    remove unused code

--- src/DB/appwrite.config.js
+++ src/DB/appwrite.config.js
@@ -6,23 +6,6 @@ client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject(process.env.REACT_APP_PROJECT_ID);
 
-// const permissions = [
-//   Permission.read(Role.user(user['$id'])),
-//   Permission.write(Role.user(user['$id'])),
-// ]
-
-
-// const resp = await db.createDocument(
-//   palettegramDB,
-//   usersCollection,
-//   ID.unique(),
-//   {
-//     email: userData.email,
-//     fullName: userData.fullName,
-//     username: userData.username,
-//   }
-// );
-// const db = new Databases(client);
 
 const account = new Account(client);
 const db = new Databases(client);
