e10204bfa1040741f265aedeef1379e5ef39fad5
SahilGill003 <gillsahil003@gmail.com>
2023-06-05 12:36:25 +0530

    merged and removed some code from Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,21 +5,6 @@ const Feed = () => {
   return (
     <main className="flex max-w-screen-lg mx-auto pt-8 content-center">
       <div className="flex-[1] h-80 sticky top-24 flex flex-col items-end">
-        {/* FIX USER AND BOOKMARK ICONS */}
-        {/* <div className="rounded-[100px] shadow-lg h-32 w-32 bg-slate-100 mb-4 relative"> */}
-        {/* <i className="fa-3x absolute "></i>
-        </div>
-        <div className="rounded-[50px] shadow-lg bg-slate-100  ">
-          <i className="fa-2x"></i>
-        </div> */}
-        {/* <div className="flex flex-col items-center gap-4 ">
-          <div className="rounded-full shadow-xl  ">
-            <i className="fa fa-user "></i>
-          </div>
-          <div>
-            <i className="fa fa-bookmark  "></i>
-          </div>
-        </div> */}
         <div className="w-16 h-16 rounded-full bg-red-500 flex items-center justify-center m-8 mt-0">
           <i className="fa fa-user fa-2x"></i>
         </div>
