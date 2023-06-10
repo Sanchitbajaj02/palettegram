ce250502d862ae7be9c012d43a52e33581702023
Sahil Gill <65935046+SahilGill003@users.noreply.github.com>
2023-06-10 15:53:59 +0530

    fix: Feed page UI Bugs #28 (#30)
    * created basic feed page
    
    * D
    
    * "Added posts"
    
    This reverts commit ea02f7e801414dbb8f4012a37bd08ccc3ff7cd65.
    
    * update the eslint configuration
    
    * remove junk configuration
    
    * fix the eslint bug
    
    * CreatePost and Profile page changes
    
    * individual post changes
    
    * error fix
    
    * Name changed
    
    * PR issues resolved
    
    * Added some fields
    
    * Fixed feed page icons
    
    * changed file names
    
    * PostProfilePage changes
    
    * Feed changes
    
    * changes
    
    * merged and removed some code from Feed
    
    * Added Basic Redux setup
    
    * storing register and login details in redux
    
    * feed and post inaccessibility to unregistered users
    
    * Added some styling
    
    * removed border
    
    * Restored env.example
    
    ---------
    
    Co-authored-by: Sanchit Bajaj <sanchit02@outlook.com>

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import CreatePost from "./CreatePost";
 import Posts from "./Posts";
 import TrendingFeed from "./TrendingFeed";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
@@ -22,15 +23,19 @@ const Feed = () => {
   return (
     <main className="flex max-w-screen-lg mx-auto pt-8 content-center">
       <div className="flex-[1] h-80 sticky top-24 flex flex-col items-end">
-        <div className="w-16 h-16 rounded-full bg-red-500 flex items-center justify-center m-8 mt-0">
-          <i className="fa fa-user fa-2x"></i>
-        </div>
-        <div className="w-16 h-16 rounded-full bg-yellow-300 flex items-center justify-center m-8 mt-0">
-          <i className="fa fa-bookmark fa-2x "></i>
-        </div>
+        <Link to="/feed">
+          <div className="w-12 h-12 rounded-full border hover:bg-black hover:text-white flex items-center justify-center m-8 mt-0">
+            <i className="fa fa-user fa-lg"></i>
+          </div>
+        </Link>
+        <Link to="/feed">
+          <div className="w-12 h-12 rounded-full border hover:bg-black hover:text-white flex items-center justify-center m-8 mt-0">
+            <i className="fa fa-bookmark fa-lg scale-[0.9]"></i>
+          </div>
+        </Link>
       </div>
       <div className="flex flex-[3] gap-2">
-        <div className="flex-1 flex flex-col border border-blue-600  overflow-y-scroll ">
+        <div className="flex-1 flex flex-col border border-blue-600">
           <CreatePost />
           <Posts />
         </div>
