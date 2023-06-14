8ed12428e7db7d7882f83938d6146dbd4a0d0c88
kunal <10kunalji@gmail.com>
2023-06-14 21:00:19 +0530

    feat: added user like

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import SinglePost from "./SinglePost.jsx";
-import { getAllPosts } from "../DB/api.js";
+import { getAllPosts, likeTweet } from "../DB/api.js";
+import { useSelector } from "react-redux";
 const Posts = () => {
+  const registerDetails = useSelector((state) => state.authenticator);
   const [allPosts, setAllPosts] = useState([]);
   useEffect(() => {
     getAllPosts()
@@ -12,6 +14,21 @@ const Posts = () => {
         console.log(err);
       });
   }, [allPosts]);
+  const likePost = async (post) => {
+    if (post.likes.includes(registerDetails.userId)) {
+      post.likes.pop(registerDetails.userId);
+    } else {
+      post.likes.push(registerDetails.userId);
+    }
+
+    likeTweet(post)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const tweetsSortedByCreatedDate = allPosts.sort(function (a, b) {
     return new Date(b.$createdAt) - new Date(a.$createdAt);
@@ -20,7 +37,7 @@ const Posts = () => {
     <div className="flex flex-col items-center w-full">
       {tweetsSortedByCreatedDate.map((posts, index) => (
         <div key={index} className="w-full">
-          <SinglePost singlePost={posts} />
+          <SinglePost singlePost={posts} onLikeClick={likePost} />
         </div>
       ))}
     </div>
