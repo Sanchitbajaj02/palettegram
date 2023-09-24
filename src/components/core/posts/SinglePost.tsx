6040b2300d60f94fc5d6c188309bb6b37b149d43
Sanchit Bajaj <55249639+Sanchitbajaj02@users.noreply.github.com>
2023-09-24 21:06:13 +0300

    fix: ui updates / code refactor (#59)
    * docs: readme update with enhanced grammar
    
    * docs: updated the images of readme
    
    * fix: create post and show post system created
    
    * refactor: code refactored with color options and updated the footer component
    
    * docs: yaml updated for pr checker

---
+++ src/components/core/posts/SinglePost.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable quotes */
+import Link from "next/link";
+import Image from "next/image";
+import { Download, Heart, MessageCircle, Share, Bookmark } from "react-feather";
+import { useSelector } from "react-redux";
+
+// eslint-disable-next-line react/prop-types
+const SinglePost = ({ singlePost, onLikeClick }: { singlePost: any; onLikeClick: any }) => {
+  const post = singlePost;
+
+  const authState = useSelector((state: any) => state.authenticator);
+
+  const copyText = async (color: string) => {
+    await navigator.clipboard.writeText(color);
+  };
+
+  return (
+    <div className="p-2 rounded-md shadow-sm mb-4">
+      <Link className="flex items-center gap-3 mb-3" href={`/user/${post?.userId}`}>
+        <div className="w-12 h-12 rounded-full border flex items-center justify-center shadow">
+          <Image src="/assets/user.png" alt="user" width={40} height={40} />
+        </div>
+        <span className="font-medium text-md">{post?.userId}</span>
+      </Link>
+      <Link href={`/post/${post?.$id}`}>
+        <p className="text-md mb-4">{post?.postTitle ? post?.postTitle : "No Title"}</p>
+
+        {post?.postImage[0]?.length > 0 ? (
+          <Image
+            className="w-full mb-4"
+            src={post?.postImage[0]}
+            alt={post?.postTitle}
+            width={400}
+            height={200}
+          />
+        ) : null}
+      </Link>
+
+      {post?.colors?.length > 0 ? (
+        <div className="my-2 flex flex-row justify-between items-center w-full">
+          {post?.colors.map((color: string, index: string) => {
+            return (
+              <div
+                key={index}
+                className="flex-grow h-52 cursor-pointer  transition duration-200"
+                onClick={() => copyText(`#${color}`)}
+                style={{
+                  backgroundColor: `#${color}`,
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      ) : null}
+
+      <div className="flex justify-around">
+        <article
+          onClick={() => onLikeClick(post)}
+          className={`flex flex-row gap-3 items-center transition ease-in-out duration-200 hover:cursor-pointer ${
+            post?.likes.includes(authState?.userId)
+              ? "text-primary hover:text-primary"
+              : "text-secondary-light dark:text-white hover:text-primary"
+          }`}
+        >
+          <Heart
+            size={22}
+            fill="true"
+            className={`${
+              post?.likes.includes(authState?.userId) ? "fill-primary" : "fill-transparent"
+            }`}
+          />
+          <span className="text-base">{post?.likes.length}</span>
+        </article>
+
+        <article className="flex flex-row gap-3 items-center transition ease-in-out duration-200 hover:cursor-pointer text-secondary-light dark:text-white hover:text-primary">
+          <MessageCircle size={22} />
+          <span className="text-base">{post?.comments.length}</span>
+        </article>
+
+        <article className="flex flex-row gap-3 items-center transition ease-in-out duration-200 hover:cursor-pointer text-secondary-light dark:text-white hover:text-primary">
+          <Bookmark size={22} />
+        </article>
+
+        <article className="flex flex-row gap-3 items-center transition ease-in-out duration-200 hover:cursor-pointer text-secondary-light dark:text-white hover:text-primary">
+          <Share size={22} />
+        </article>
+
+        <article className="flex flex-row gap-3 items-center transition ease-in-out duration-200 hover:cursor-pointer text-secondary-light dark:text-white hover:text-primary">
+          <Download size={22} />
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePost;
--- src/components/pages/feed/index.tsx
+++ src/components/pages/feed/index.tsx
@@ -1,41 +1,45 @@
 "use client";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { User, Bookmark } from "react-feather";
+import { useRouter } from "next/navigation";
+
 import CreatePost from "@/components/core/createPost";
 import Posts from "@/components/core/posts";
 import TrendingFeed from "@/components/core/trendingFeed";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { User, Bookmark } from "react-feather";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import Footer from "@/components/core/footer";
 
 const Feed = () => {
   const userDetails = useSelector((state: any) => state.auth);
   const router = useRouter();
 
   return (
-    <main className="flex max-w-screen-lg mx-auto pt-8 content-center">
-      <div className="flex-1 sticky flex flex-col items-center gap-8">
-        <Link
-          href={`/user/${userDetails.creds.userId}`}
-          className="w-12 h-12 rounded-full flex items-center justify-center shadow-md dark:shadow-gray-600 transition-all duration-300 text-black dark:text-white hover:text-primary-light border hover:border-primary-light"
-        >
-          <User size={20} />
-        </Link>
-        <Link
-          href="/user/bookmarks"
-          className="w-12 h-12 rounded-full flex items-center justify-center shadow-md dark:shadow-gray-600 transition-all duration-300 text-black dark:text-white hover:text-primary-light border hover:border-primary-light"
-        >
-          <Bookmark size={20} />
-        </Link>
-      </div>
-      <div className="flex-[5]">
-        <CreatePost />
-        <Posts />
-      </div>
-      <div className="flex-[2] hidden md:block">
-        <TrendingFeed />
-      </div>
-    </main>
+    <>
+      <main className="flex max-w-screen-lg mx-auto pt-8 content-center">
+        <div className="flex-1 sticky flex flex-col items-center gap-8">
+          <Link
+            href={`/user/${userDetails.creds.userId}`}
+            className="w-12 h-12 rounded-full flex items-center justify-center shadow-md dark:shadow-gray-600 transition-all duration-300 text-black dark:text-white hover:text-primary-light border hover:border-primary-light"
+          >
+            <User size={20} />
+          </Link>
+          <Link
+            href="/user/bookmarks"
+            className="w-12 h-12 rounded-full flex items-center justify-center shadow-md dark:shadow-gray-600 transition-all duration-300 text-black dark:text-white hover:text-primary-light border hover:border-primary-light"
+          >
+            <Bookmark size={20} />
+          </Link>
+        </div>
+        <div className="flex-[5]">
+          <CreatePost />
+          <Posts />
+        </div>
+        <div className="flex-[2] hidden md:block">
+          <TrendingFeed />
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 };
 export default Feed;
