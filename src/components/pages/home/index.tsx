8df5bf072b3ff16dadd4d61a93226b0b3fae7276
Kaybangz <devcaleb96@gmail.com>
2023-10-05 02:41:23 +0100

    Changed layout for landing page

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -16,6 +16,14 @@ function HomePage() {
   const state = useSelector((state: any) => state.auth);
 
   const [stars, setStars] = useState(0);
+ 
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  
+  const handleMenuClick = () => {
+      setMenuOpen(!isMenuOpen);
+    };
+
+  
 
   useEffect(() => {
     fetch("https://api.github.com/repos/sanchitbajaj02/palettegram")
@@ -44,9 +52,10 @@ function HomePage() {
 
   return (
     <>
-      <nav className="py-4 shadow-md dark:shadow-gray-600">
-        <div className=" max-w-screen-xl mx-auto flex flex-row justify-between items-center">
-          <Link href="/">
+    
+    <nav className="py-4 shadow-md dark:shadow-gray-600">
+      <div className="max-w-screen-lg mx-auto flex flex-row justify-between items-center">
+        <Link href="/">
             <Image
               src="/assets/logo.png"
               alt="profile logo"
@@ -54,19 +63,43 @@ function HomePage() {
               height={50}
               className="dark:shadow-md dark:shadow-gray-500 rounded-full"
             />
-          </Link>
-          <div className="flex items-center justify-center">
-            <Theme />
-            <Link
+        </Link>
+
+        <div className="md:hidden">
+        <Theme />
+        </div>
+        
+
+        {/* Hamburger menu button for small screens */}
+        <div className="md:hidden flex items-center">
+          <button
+          onClick={handleMenuClick}
+          className={`lg:hidden dark:text-white dark:hover:text-primary focus:outline-none ${isMenuOpen ? 'hidden' : 'closed'} text-gray-600 hover:text-primary`}
+          >
+              <svg
+                className="w-10 h-10 z-50"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+         
+          </button>
+        </div>
+
+        {/* Desktop menu items */}
+        <div className="hidden md:flex items-center justify-center space-x-4">
+          <Theme />
+          <Link
               href="https://github.com/Sanchitbajaj02/palettegram"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center text-md mx-2 px-8 py-2 rounded-full bg-primary text-white"
             >
               <GitHub size={20} className="mr-4" /> {stars} Stars
-            </Link>
-
-            {!state?.creds.userId && !state?.creds.isVerified && (
+          </Link>
+          {!state?.creds.userId && !state?.creds.isVerified && (
               <>
                 <Link
                   href="/register"
@@ -82,32 +115,87 @@ function HomePage() {
                   Login
                 </Link>
               </>
+          )}
+        </div>
+        
+      {/* Mobile menu */}
+      {isMenuOpen && (
+        <div className="bg-primary bg-opacity-50 h-full z-10 w-1/2 fixed inset-y-0 right-0 md:hidden">
+          <button
+          onClick={handleMenuClick}
+          className={`absolute right-0 top-5 dark:text-white dark:hover:text-primary focus:outline-none ${isMenuOpen ? 'open' : 'closed'} text-gray-600 hover:text-primary lg:hidden`}
+          >
+              <svg
+                className="w-10 h-10 z-50"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+          </button>
+
+          <div className="grid grid-cols-1 gap-10 mt-24"> 
+            <Link
+              href="https://github.com/Sanchitbajaj02/palettegram"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-sm mx-2 px-6 py-2 rounded-full bg-primary text-white"
+            >
+              <GitHub size={20} className="mr-4" /> {stars} Stars
+            </Link>
+
+            {!state?.creds.userId && !state?.creds.isVerified && (
+              <>
+                <Link
+                  href="/register"
+                  className="inline-block mx-2 px-6 py-2 text-sm rounded-full text-white bg-primary"
+                >
+                  Register
+                </Link>
+
+                <Link
+                  href="/login"
+                  className="inline-block mx-2 px-6 py-2 text-sm rounded-full text-white bg-primary"
+                >
+                  Login
+                </Link>
+
+                
+                
+              </>
             )}
           </div>
-        </div>
-      </nav>
 
-      <main className="max-w-screen-xl mx-auto px-2">
-        <section className="flex items-center flex-col md:flex-row gap-4 mt-20 mb-24">
+        </div>
+        )}
+      </div>
+    </nav>
+
+      <main className="max-w-screen-lg mx-auto px-2">
+        <section className="flex flex-col items-center mt-20 mb-24 gap-4 md:flex-row md:justify-between">
           <article>
-            <h1 className="text-3xl md:text-6xl font-extrabold tracking-wide text-black dark:text-white">
+            <h1 className="text-4xl md:text-6xl text-center font-extrabold tracking-wide text-black dark:text-white md:text-left">
               Present Palettes Around the World
             </h1>
-            <p className="text-xl md:text-2xl my-8 text-black dark:text-white">
+            <p className="text-2xl md:text-3xl my-8 text-black text-center dark:text-white md:text-left">
               Transform ideas into Beautiful Palettes, Inspire Fellow Designers.
             </p>
+
+            <div className="flex justify-center md:justify-start">
             {state?.creds.userId && state?.creds.isVerified ? (
-              <Link href="/feed" className="px-16 py-4 text-lg rounded-full text-white bg-primary">
+              <Link href="/feed" className="px-10 py-3 text-sm rounded-full text-white bg-primary md:px-16 md:py-4 md:text-lg">
                 Checkout your feed
               </Link>
             ) : (
               <Link
                 href="register"
-                className="px-16 py-4 text-lg rounded-full text-white bg-primary"
+                className="px-10 py-2 text-md rounded-full text-white bg-primary md:px-16 md:py-4 md:text-lg"
               >
                 Start your journey
               </Link>
             )}
+            </div>
           </article>
           <figure className="w-[80%] my-4">
             <div className="shape1 -z-10" />
@@ -122,8 +210,8 @@ function HomePage() {
           </figure>
         </section>
 
-        <section className="flex items-center flex-col-reverse md:flex-row gap-4 mt-24 mb-20">
-          <figure className="w-full md:w-[50%]">
+        <section className="flex items-center flex-col-reverse md:flex-row gap-4 mt-30 mb-20">
+          <figure className="w-[50%] md:w-[50%]">
             <Image
               src="/assets/palettegram_for.png"
               alt="Who is palettegram for section"
@@ -134,10 +222,10 @@ function HomePage() {
             />
           </figure>
           <article>
-            <h1 className="text-3xl md:text-6xl font-extrabold tracking-wide text-black dark:text-white">
+            <h1 className="text-3xl md:text-6xl text-center font-extrabold tracking-wide text-black dark:text-white md:text-right">
               Who is Palettegram for?
             </h1>
-            <p className="text-xl md:text-2xl my-8 text-black dark:text-white">
+            <p className="text-xl text-center md:text-2xl my-8 text-black dark:text-white md:text-right">
               Anyone who wants to share their designs and color palettes to get the review among the
               professionals.
             </p>
