8a0295336a76a907b01b58ade972bf62903dace5
Irfan shaik <414irfann@gmail.com>
2024-01-02 13:36:18 +0530

    fix4

--- src/app/feed/loading.tsx
+++ src/app/feed/loading.tsx
@@ -1,8 +1,8 @@
 export default async function Loading() {
   return (
     <>
-      <main className="flex max-w-screen-lg mx-auto pt-24 content-center">
-        <div className="flex-1 sticky flex flex-col items-center gap-8">
+      <main className="flex max-w-screen-lg mx-auto content-center pt-24">
+        <div className="flex-1 sticky flex flex-col gap-8">
           <div className="w-12 h-12 rounded-full dark:bg-white/10 bg-black/10 animate-pulse"></div>
           <div className="w-12 h-12 rounded-full bg-black/10 dark:bg-white/20 animate-pulse"></div>
         </div>
