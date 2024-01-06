f40fd8fb00af85d9588b9fd51ea30a43f7c50641
Archit <architgargagra@gmail.com>
2024-01-07 02:18:11 +0530

    feat: custom 404 page

--- src/app/[others]/page.tsx
+++ src/app/[others]/page.tsx
@@ -1,31 +1,23 @@
-
 import { ButtonLong } from "@/components/core/buttons";
 import Footer from "@/components/core/footer";
 import Navbar from "@/components/core/navbar";
 
 export default function Custom404() {
-    return (
-      <>
-        <Navbar/>
+  return (
+    <>
+      <Navbar />
       <div className=" flex flex-col items-center justify-center h-[80vh]">
-      
+        <h1 className=" py-5 font-bold text-2xl"> Error 404: Page does'nt Exist! </h1>
+        <p className=" font-semibold mb-5">Please check the url again</p>
+        <ButtonLong href="/" size="big">
+          Get Back To Home
+        </ButtonLong>
+        <div></div>
 
-      
-        
-      <h1 className=" py-5 font-bold text-2xl"> Error 404: Page does'nt Exist! </h1>
-      <p className=" font-semibold mb-5">Please check the url again</p>
-      <ButtonLong href="/" size="big">Get Back To Home</ButtonLong>
-      <div>
-
+        <div className=" bottom-0 fixed w-full">
+          <Footer />
+        </div>
       </div>
-
-
-<div className=" bottom-0 fixed w-full" >
-     <Footer/>
-</div>
-      </div>
-      
-      </>
-      
-    )
-  }
+    </>
+  );
+}
