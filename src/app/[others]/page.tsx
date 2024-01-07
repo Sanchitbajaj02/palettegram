44ada31bf5b3e466270179b8349a1b2c81e970a5
Sanchit Bajaj <55249639+Sanchitbajaj02@users.noreply.github.com>
2024-01-07 18:14:47 +0530

    docs: fix the docs and fix minor changes (#142)
    * refactor: minor refactoring in files
    
    * docs: yml fix for SWOC

--- src/app/[others]/page.tsx
+++ src/app/[others]/page.tsx
@@ -7,8 +7,8 @@ export default function Custom404() {
     <>
       <Navbar />
       <div className=" flex flex-col items-center justify-center h-[80vh]">
-        <h1 className=" py-5 font-bold text-2xl"> Error 404: Page does'nt Exist! </h1>
-        <p className=" font-semibold mb-5">Please check the url again</p>
+        <h1 className="py-5 font-semibold text-4xl"> Error 404: Page does not Exist! </h1>
+        <h2 className="mb-5 text-2xl">Please check the url again</h2>
         <ButtonLong href="/" size="big">
           Get Back To Home
         </ButtonLong>
