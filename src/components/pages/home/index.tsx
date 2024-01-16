6b65b6238670919388379b68387474bebc80e28b
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-01-16 21:58:06 +0530

    style: add animation effects

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -11,6 +11,7 @@ import { Menu, X } from "react-feather";
 import ThemeButton from "@/components/core/themeButton";
 import { ButtonLong } from "@/components/core/buttons";
 import { parseCookies } from "nookies";
+import { motion } from "framer-motion"
 
 function HomePage() {
   const router = useRouter();
@@ -58,13 +59,13 @@ function HomePage() {
       <nav className="py-4 shadow-md dark:shadow-gray-600">
         <div className="max-w-screen-lg mx-auto flex flex-row justify-between items-center">
           <Link href="/">
-            <Image
-              src="/assets/logo.png"
-              alt="profile logo"
-              width={50}
-              height={50}
-              className="dark:shadow-md dark:shadow-gray-500 rounded-full ml-2 md:ml-0"
-            />
+              <Image
+                src="/assets/logo.png"
+                alt="profile logo"
+                width={50}
+                height={50}
+                className="dark:shadow-md dark:shadow-gray-500 rounded-full ml-2 md:ml-0"
+              />
           </Link>
 
           {/* Hamburger menu button for small screens */}
@@ -80,7 +81,15 @@ function HomePage() {
           </div>
 
           {/* Desktop menu items */}
-          <div className="hidden md:flex items-center justify-center space-x-4">
+          <motion.div initial={{ opacity: 0, y: -100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              duration: 0.5,
+              type: "spring",
+              stiffness: 80,
+            }}
+             className="hidden md:flex items-center justify-center space-x-4">
             <ThemeButton iconSize={24} />
 
             <ButtonLong href="https://github.com/Sanchitbajaj02/palettegram" newTab size="normal">
@@ -100,7 +109,7 @@ function HomePage() {
                 </ButtonLong>
               </>
             )}
-          </div>
+          </motion.div>
 
           {/* Mobile menu */}
           {isMenuOpen && (
@@ -162,14 +171,41 @@ function HomePage() {
       <main className="max-w-screen-lg mx-auto px-2">
         <section className="flex flex-col items-center mt-32 mb-32 gap-4 md:flex-row md:justify-between">
           <article>
-            <h1 className="text-3xl font-bold text-center tracking-wide text-secondary dark:text-white md:text-6xl md:text-left">
-              Present Palettes Around the World
-            </h1>
-            <p className="text-xl my-6 font-medium text-center text-secondary dark:text-white md:text-2xl md:text-left">
-              Transform ideas into Beautiful Palettes, Inspire Fellow Designers.
-            </p>
+            <motion.h1 initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                duration: 1,
+                type: "spring",
+                stiffness: 110,
+                delay: 0,
+              }}
+               className="text-3xl font-bold text-center tracking-wide text-secondary dark:text-white md:text-6xl md:text-left">
+                Present Palettes Around the World
+            </motion.h1>
+            <motion.p initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                duration: 0.5,
+                type: "spring",
+                stiffness: 110,
+                delay: 0.1,
+              }}
+              className="text-xl my-6 font-medium text-center text-secondary dark:text-white md:text-2xl md:text-left">
+                Transform ideas into Beautiful Palettes, Inspire Fellow Designers.
+            </motion.p>
 
-            <div className="flex justify-center md:justify-start">
+            <motion.div initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                duration: 0.5,
+                type: "spring",
+                stiffness: 110,
+                delay: 0.2,
+              }}
+              className="flex justify-center md:justify-start">
               {state?.creds.userId && state?.creds.userId !== "" ? (
                 <ButtonLong href="/feed" size="big">
                   Checkout your feed
@@ -179,40 +215,81 @@ function HomePage() {
                   Start your journey
                 </ButtonLong>
               )}
-            </div>
+            </motion.div>
           </article>
 
           <figure className="w-[70%] my-4 text-center">
-            <Image
-              src="/assets/header.png"
-              alt="Header section"
-              loading="lazy"
-              width={400}
-              height={400}
-              className="custom-shadow"
-            />
+            <motion.div 
+            initial={{ opacity: 0, y: -640 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              duration: 0.5,
+              type: "spring",
+              stiffness: 80,
+            }}
+            >
+              <Image
+                src="/assets/header.png"
+                alt="Header section"
+                loading="lazy"
+                width={400}
+                height={400}
+                className="custom-shadow floating-image"
+              />
+            </motion.div>
           </figure>
         </section>
 
         <section className="flex items-center flex-col-reverse md:flex-row gap-4 mt-32 mb-32">
           <figure className="w-[70%]">
-            <Image
-              src="/assets/palettegram-for.png"
-              alt="Who is palettegram for section"
-              loading="lazy"
-              width={600}
-              height={500}
-              className="mx-auto rounded"
-            />
+          <motion.div 
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true }}
+            transition={{
+              duration: 0.5,
+              type: "spring",
+              stiffness: 80,
+              delay: 0.2,
+            }}
+            >
+              <Image
+                src="/assets/palettegram-for.png"
+                alt="Who is palettegram for section"
+                loading="lazy"
+                width={600}
+                height={500}
+                className="mx-auto rounded"
+              />
+            </motion.div>
           </figure>
           <article>
-            <h1 className="text-3xl md:text-6xl text-center font-extrabold tracking-wide text-black dark:text-white md:text-right">
-              Who is <br /> Palettegram for?
-            </h1>
-            <p className="text-xl md:text-2xl text-center my-8 text-black dark:text-white md:text-right">
+            <motion.h1 initial={{ opacity: 0, x: 100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                duration: 1,
+                type: "spring",
+                stiffness: 80,
+                delay: 0,
+              }}
+              className="text-3xl md:text-6xl text-center font-extrabold tracking-wide text-black dark:text-white md:text-right">
+                Who is <br /> Palettegram for?
+            </motion.h1>
+            <motion.p initial={{ opacity: 0, x: 100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                duration: 0.5,
+                type: "spring",
+                stiffness: 80,
+                delay: 0.1,
+              }}
+              className="text-xl md:text-2xl text-center my-8 text-black dark:text-white md:text-right">
               Anyone who wants to share their designs and color palettes to get the review among the
               professionals.
-            </p>
+            </motion.p>
           </article>
         </section>
       </main>
