eb2eb8a05ce3eaf05e8ccb8d19c6c4768a31015c
Sanchit Bajaj <sanchit02@outlook.com>
2024-01-20 18:22:43 +0530

    chore: fixed package.json and improve minor fixes

--- appwrite-gen/src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from "fs";
-import { Data, Schema } from "./types";
-
-export const getSchemaFromFile = () => {
-  const fileContent = readFileSync("schema.json", { encoding: "utf-8" });
-  const schema: Schema = JSON.parse(fileContent);
-  return schema;
-};
