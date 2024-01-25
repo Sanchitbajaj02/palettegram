7dc48ad399421bc350db646deb887774b6148748
jaymehta002 <jaymehta002@gmail.com>
2024-01-25 21:20:21 +0530

    style: added back arrow with react-feather library

--- src/components/pages/post/index.tsx
+++ src/components/pages/post/index.tsx
@@ -4,6 +4,8 @@ import { Suspense } from "react";
 import SinglePost from "@/components/core/posts/SinglePost";
 import TrendingFeed from "@/components/core/trendingFeed";
 import { PostInstanceType } from "@/types";
+import { ArrowLeft } from "react-feather";
+
 
 export default function PostById({ singlePostInfo }: { singlePostInfo: PostInstanceType }) {
   const router = useRouter();
@@ -12,7 +14,8 @@ export default function PostById({ singlePostInfo }: { singlePostInfo: PostInsta
     <main className="flex sm:flex-row flex-col max-w-screen-lg mx-auto pt-8 content-center px-4">
       <div className="flex-[1]">
         <button onClick={() => router.back()} className="text-primary hover:text-primary-light">
-          {"< Back"}
+          <ArrowLeft size={20} className="mr-2" />
+          {"Back"}
         </button>
       </div>
       <div className="flex-[5] mt-4 sm:mt-0">
