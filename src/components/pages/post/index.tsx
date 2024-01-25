eae03d52b80b455c07b1d6bec8bc9b93556891ae
jaymehta002 <jaymehta002@gmail.com>
2024-01-25 17:56:30 +0530

    added-back-button

--- src/components/pages/post/index.tsx
+++ src/components/pages/post/index.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { Suspense } from "react";
 import SinglePost from "@/components/core/posts/SinglePost";
 import TrendingFeed from "@/components/core/trendingFeed";
 import { PostInstanceType } from "@/types";
 
 export default function PostById({ singlePostInfo }: { singlePostInfo: PostInstanceType }) {
+  const router = useRouter();
+
   return (
     <main className="flex sm:flex-row flex-col max-w-screen-lg mx-auto pt-8 content-center px-4">
+      <div className="flex-[1]">
+        <button onClick={() => router.back()} className="text-primary hover:text-primary-light">
+          {"< Back"}
+        </button>
+      </div>
       <div className="flex-[5] mt-4 sm:mt-0">
         <SinglePost singlePost={singlePostInfo} />
       </div>
