5bca7b6c71b46c6fe24f64f10c88526888546cfe
girishbari <barigirish50@gmail.com>
2024-02-11 20:52:55 +0530

    feat: replaced react-toastify with sonner

--- src/helper/toastify.ts
+++ src/helper/toastify.ts
@@ -1,18 +1,34 @@
 import "react-toastify/dist/ReactToastify.css";
 
-import { toast } from "react-toastify";
-import type { TypeOptions } from "react-toastify";
+import { toast } from "sonner";
+
+// import { toast } from "react-toastify";
+// import type { TypeOptions } from "react-toastify";
+
+type ToastTypes = 'success' | 'info' | 'warning' | 'error';
+
 
 /**
  * @work create a toastify by parsing the following values
  * @param {String} text
  * @param {TypeOptions} statusType
- * @param {boolean} loadingStatus
  * @returns
  */
-export function toastify(text: string, statusType: TypeOptions, loadingStatus: boolean = false) {
-  return toast(text, {
-    type: statusType,
-    isLoading: loadingStatus,
-  });
+
+
+
+export function toastify(text: string, statusType: ToastTypes) {
+  if (statusType == 'success') {
+    return toast.success(text)
+  }
+  if (statusType == 'info') {
+    return toast.info(text)
+  }
+  if (statusType == 'error') {
+    return toast.error(text)
+  }
+  // return toast(text, {
+  //   type: statusType,
+  //   isLoading: loadingStatus,
+  // });
 }
