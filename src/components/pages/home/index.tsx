e098c660b07479d8b5689667bbc92a6ab1718c56
Girish Bari (He/Him) <38005544+Girishbari@users.noreply.github.com>
2024-02-18 23:22:14 +0530

    fix: navbar bug (#251)
    * refactor: solved the bug mentioned in #246
    
    * feat: replaced react-toastify with sonner lib (#242)
    
    * feat: new changes
    
    * feat: replaced react-toastify with sonner
    
    * feat: yarn file
    
    * refactor: added requested changes
    
    ---------
    
    Co-authored-by: Sanchit Bajaj <55249639+Sanchitbajaj02@users.noreply.github.com>
    
    * feat: Form validation (#243)
    
    * feat: add detailed footer section
    
    * fix: form-validation bug
    
    * fix: changed footer alignment (#239)
    
    * fix: patch global scrollbar bug (#250)
    
    * Random avatar generator for profiles #154 (#207)
    
    Co-authored-by: Sanchit Bajaj <55249639+Sanchitbajaj02@users.noreply.github.com>
    
    * refactor: patch navbar bug
    
    * refactor: patch navbar bug 2
    
    ---------
    
    Co-authored-by: Sanchit Bajaj <55249639+Sanchitbajaj02@users.noreply.github.com>
    Co-authored-by: Monu <142221687+monoMonu@users.noreply.github.com>
    Co-authored-by: Kom Senapati <komnoob123@gmail.com>
    Co-authored-by: Amit Mishra <65525769+DaSeeker67@users.noreply.github.com>

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,30 +1,20 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Link from "next/link";
 import Image from "next/image";
-import { GitHub, User } from "react-feather";
 import { getCurrentUser } from "@/backend/auth.api";
 import { saveUser } from "@/redux/reducers/authReducer";
-import { Menu, X } from "react-feather";
-import ThemeButton from "@/components/core/themeButton";
 import { ButtonLong } from "@/components/core/buttons";
 import { parseCookies } from "nookies";
 import { toastify } from "@/helper/toastify";
 import { motion } from "framer-motion";
 
-function HomePage({ starCount }: { starCount: number }) {
+function HomePage() {
   const dispatch = useDispatch();
   const cookies = parseCookies();
   const accountIdFromCookies: string = cookies["accountId"];
 
   const state = useSelector((state: any) => state.auth);
-
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
-  const handleMenuClick = () => {
-    setMenuOpen(!isMenuOpen);
-  };
 
   useEffect(() => {
     if (accountIdFromCookies) {
@@ -51,120 +41,6 @@ function HomePage({ starCount }: { starCount: number }) {
 
   return (
     <>
-      <nav className="py-4 shadow-md dark:shadow-gray-600">
-        <div className="max-w-screen-lg mx-auto flex flex-row justify-between items-center">
-          <Link href="/">
-            <Image
-              src="/assets/logo.png"
-              alt="profile logo"
-              width={50}
-              height={50}
-              className="dark:shadow-md dark:shadow-gray-500 rounded-full ml-2 md:ml-0"
-            />
-          </Link>
-
-          {/* Hamburger menu button for small screens */}
-          <div className="md:hidden flex items-center">
-            <button
-              onClick={handleMenuClick}
-              className={`px-2 lg:hidden dark:text-white dark:hover:text-primary text-secondary hover:text-primary transition-all duration-300 focus:outline-none ${
-                isMenuOpen ? "hidden" : "closed"
-              }`}
-            >
-              <Menu size={32} />
-            </button>
-          </div>
-
-          {/* Desktop menu items */}
-          <motion.div
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{
-              duration: 0.5,
-              type: "spring",
-              stiffness: 80,
-            }}
-            className="hidden md:flex items-center justify-center space-x-4"
-          >
-            <ThemeButton iconSize={24} />
-
-            <ButtonLong href="https://github.com/Sanchitbajaj02/palettegram" newTab size="normal">
-              <span className="flex items-center">
-                <GitHub size={20} className="mr-2" /> {starCount} Stars
-              </span>
-            </ButtonLong>
-
-            {!state?.creds.userId && !state?.creds.isVerified && (
-              <>
-                <ButtonLong href="/register" size="normal">
-                  Register
-                </ButtonLong>
-
-                <ButtonLong href="/login" size="normal">
-                  Login
-                </ButtonLong>
-              </>
-            )}
-          </motion.div>
-
-          {/* Mobile menu */}
-          {isMenuOpen && (
-            <div className="bg-secondary-light bg-opacity-25 dark:bg-primary-light dark:bg-opacity-25 h-full z-10 w-1/2 fixed inset-y-0 right-0 md:hidden transition-transform duration-300">
-              <button
-                onClick={handleMenuClick}
-                className={`absolute right-2 top-6 dark:text-white dark:hover:text-primary focus:outline-none ${
-                  isMenuOpen ? "open" : "closed"
-                } text-gray-600 hover:text-primary lg:hidden`}
-              >
-                <X size={32} />
-              </button>
-
-              <div className="grid grid-cols-1 gap-10 mt-24">
-                <div className="text-center">
-                  <ThemeButton iconSize={24} />
-                </div>
-
-                <Link
-                  href="https://github.com/Sanchitbajaj02/palettegram"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center text-sm text-center mx-2 px-6 py-2 rounded-full bg-primary text-white"
-                >
-                  <GitHub size={20} className="mr-4" /> {starCount} Stars
-                </Link>
-
-                {!state?.creds.userId && !state?.creds.isVerified && (
-                  <>
-                    <Link
-                      href="/register"
-                      className="inline-block mx-2 px-6 py-2 text-sm rounded-full text-white bg-primary text-center"
-                    >
-                      Register
-                    </Link>
-
-                    <Link
-                      href="/login"
-                      className="inline-block mx-2 px-6 py-2 text-sm rounded-full text-white bg-primary text-center"
-                    >
-                      Login
-                    </Link>
-                  </>
-                )}
-                {state?.creds.userId && state?.creds.isVerified && (
-                  <Link
-                    href={`/user/${accountIdFromCookies}`}
-                    className="mx-2 px-2 py-2 rounded-full bg-primary text-white"
-                  >
-                    <User size={22} className="transition-all duration-300 " />
-                  </Link>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-      </nav>
-
       <main className="max-w-screen-lg mx-auto px-2">
         <section className="flex flex-col items-center mt-32 mb-32 gap-4 md:flex-row md:justify-between">
           <article>
