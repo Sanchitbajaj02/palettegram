00ba332f47da5d61adf6eb307506f2a5edd07f71
Sanchit Bajaj <55249639+Sanchitbajaj02@users.noreply.github.com>
2024-02-20 12:46:11 +0530

    fix: switch case color fix on toast with close button remove

--- src/helper/toastify.ts
+++ src/helper/toastify.ts
@@ -1,9 +1,4 @@
-import "react-toastify/dist/ReactToastify.css";
-
 import { toast } from "sonner";
-
-// import { toast } from "react-toastify";
-// import type { TypeOptions } from "react-toastify";
 
 type ToastTypes = "success" | "info" | "warning" | "error";
 
@@ -18,18 +13,14 @@ export function toastify(text: string, statusType: ToastTypes) {
   switch (statusType) {
     case "success":
       return toast.success(text);
-      break;
     case "info":
       return toast.info(text);
-      break;
     case "error":
-      return toast.success(text);
-      break;
+      return toast.error(text);
     case "warning":
-      return toast.success(text);
-      break;
-    default:
-      return toast.message(text);
-      break;
+      return toast.warning(text);
+    // default:
+    //   return toast.message(text);
+    //   break;
   }
 }
