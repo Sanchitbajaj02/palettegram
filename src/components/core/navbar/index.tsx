b8d33a245b5a11c448be35a1ef0bcebfd8b135a4
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2024-05-15 13:57:33 +0530

    enhancement in navbar

--- src/components/core/navbar/index.tsx
+++ src/components/core/navbar/index.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState, useCallback } from "react";
@@ -32,9 +33,9 @@ const Navbar = ({ starCount }: { starCount?: number }) => {
     await logoutUser();
 
     dispatch(logUserOut());
-
     router.push("/");
   };
+
 
   const currentUser = useCallback(
     (userIdFromCookies: string) => {
@@ -113,11 +114,13 @@ const Navbar = ({ starCount }: { starCount?: number }) => {
 
   return (
     <>
+
       <nav className="w-full sticky top-0 shadow-md py-2 backdrop-blur-sm dark:shadow-gray-600 z-50">
 
         {/* Desktop menu items */}
 
         <div className="max-w-screen-lg mx-auto flex items-center content-center justify-between backdrop-blur-sm bg-grey-100 bg-opacity-20 h-16 my-2">
+
           <Link href={userAuth.creds?.userId ? "/feed" : "/"}>
             <Image
               className="navbar-brand fw-bold w-10 h-10 cursor pointer dark:shadow-md dark:shadow-gray-500 rounded-full ml-2 md:ml-0 "
@@ -128,20 +131,42 @@ const Navbar = ({ starCount }: { starCount?: number }) => {
             />
           </Link>
 
+      
           {/* Hamburger menu button for small screens */}
-          <div className="md:hidden flex items-center">
-            <button
-              onClick={() => setMenuOpen(!isMenuOpen)}
-              className={`px-2 lg:hidden dark:text-white dark:hover:text-primary text-secondary hover:text-primary transition-all duration-300 focus:outline-none ${
-                isMenuOpen ? "hidden" : "closed"
-              }`}
+          <div className=" flex justify-between">
+           <div className="flex md:hidden">
+             <ThemeButton iconSize={20} />
+             </div>
+            <div>
+          <Link
+                href="https://github.com/Sanchitbajaj02/palettegram"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center text-sm mx-2 px-10 py-2 rounded-full bg-primary text-white"
+              >
+                <Github size={20} className="mr-4" /> {starCount} Stars
+              </Link>
+              </div>
+                     
+              
+
+             <div className="flex justify-between md:hidden">
+          
+              <button
+                  onClick={() => setMenuOpen(!isMenuOpen)}
+                  className={`flex px-2 dark:text-white dark:hover:text-primary text-secondary hover:text-primary transition-all duration-300 focus:outline-none ${
+                    isMenuOpen ? "hidden" : "closed"
+                  }` }
             >
               <Menu size={32} />
             </button>
           </div>
+          </div>
 
+     
+         
           <div className="hidden md:flex gap-2 flex-row items-center">
-            <ThemeButton iconSize={22} />
+         
             {userIdFromCookies ? (
               <>
                 {pathname !== "/feed" && (
@@ -174,16 +199,10 @@ const Navbar = ({ starCount }: { starCount?: number }) => {
                 </button>
               </>
             ) : (
+
+               
               <div className="hidden md:flex gap-2">
-                <ButtonLong
-                  href="https://github.com/Sanchitbajaj02/palettegram"
-                  newTab
-                  size="normal"
-                >
-                  <span className="flex items-center">
-                    <Github size={20} className="mr-2" /> {starCount} Stars
-                  </span>
-                </ButtonLong>
+             
                 <ButtonLong href="/register" size="normal">
                   Register
                 </ButtonLong>
@@ -191,6 +210,8 @@ const Navbar = ({ starCount }: { starCount?: number }) => {
                 <ButtonLong href="/login" size="normal">
                   Login
                 </ButtonLong>
+
+              <ThemeButton iconSize={20} />
               </div>
             )}
           </div>
@@ -198,7 +219,8 @@ const Navbar = ({ starCount }: { starCount?: number }) => {
 
         {/* Mobile menu */}
         {isMenuOpen && (
-          <div className="bg-secondary-light bg-opacity-25 dark:bg-primary-light dark:bg-opacity-25 h-full z-10 w-1/2 fixed inset-y-0 right-0 md:hidden transition-transform duration-300">
+          <div className="bg-secondary-light bg-opacity-25 lg:hidden dark:bg-primary-light dark:backdrop-blur-md dark:bg-opacity-25 h-full z-10 w-1/2 fixed inset-y-0 right-0 transition-transform duration-300 ">
+          
             <button
               onClick={() => setMenuOpen(!isMenuOpen)}
               className={`absolute right-2 top-6 dark:text-white dark:hover:text-primary focus:outline-none ${
@@ -208,25 +230,11 @@ const Navbar = ({ starCount }: { starCount?: number }) => {
               <X size={32} />
             </button>
 
-            <div className="grid grid-cols-1 gap-10 mt-24">
-              <div className="text-center">
-                <ThemeButton iconSize={24} />
-              </div>
-
-              <Link
-                href="https://github.com/Sanchitbajaj02/palettegram"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center text-sm mx-2 px-10 py-2 rounded-full bg-primary text-white"
-              >
-                <Github size={20} className="mr-4" /> {starCount} Stars
-              </Link>
-
-              {userAuth && userAuth.data?.$id && (
-                <>
+            <div className="grid grid-cols-1 gap-2 mt-24 h-full md:backdrop-blur-sm ">
+          
                   <Link
                     href="/register"
-                    className="inline-block mx-2 px-6 py-2 text-sm rounded-full text-white bg-primary text-center"
+                    className="mx-2 px-6 py-2 text-sm rounded-full text-white bg-primary text-center"
                   >
                     Register
                   </Link>
@@ -237,9 +245,10 @@ const Navbar = ({ starCount }: { starCount?: number }) => {
                   >
                     Login
                   </Link>
-                </>
-              )}
+             
+              
               {userAuth && userAuth.data?.$id && (
+                
                 <Link
                   href={`/user/${userAuth.data?.$id}`}
                   className="mx-2 px-2 py-2 rounded-full bg-primary text-white"
