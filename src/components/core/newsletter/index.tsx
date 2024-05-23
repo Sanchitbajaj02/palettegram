8fa30d754c3f7d088f8a13667907fe040a96555c
adesh.exc <bodead@rknec.edu>
2024-05-23 11:26:18 +0000

    feat: changes to the newsletter

--- src/components/core/newsletter/index.tsx
+++ src/components/core/newsletter/index.tsx
@@ -1,17 +1,29 @@
 "use client";
-import React from "react";
+import React , { useState } from "react";
 import { Linkedin, Facebook ,Twitter , Instagram  } from 'lucide-react';
 
 const Newsletter: React.FC = () => {
+    const [ subscriberEmail , setSubscriberEmail ] = useState('');
+
+    const handleChange = ( e : React.ChangeEvent<HTMLInputElement> )=>{
+        // const { name , value } = e.target ;
+        setSubscriberEmail(e.target.value);
+    }
+    
+    const handleSubmit = (e : React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+
+    }
+
     return <div className="w-full ">
         <div className="flex flex-col justify-center items-center  gap-6 px-20 py-10 ">
-            <h4 className="text-xs uppercase">Newsletter</h4>
-            <h2  className="text-xs md:text-base uppercase text-primary-light text-center">Sign up for latest updates and offers</h2>
-            <div className="flex flex-col gap-4 md:flex-row">
-                <input type="text" placeholder="Email Address" className="bg-white text-black py-1 px-3" ></input>
+            <h4 className="text-xs md:text-lg uppercase">Newsletter</h4>
+            <h2  className="text-xs md:text-xl uppercase text-primary-light text-center">Sign up for latest updates and offers</h2>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 md:flex-row">
+                <input onChange={handleChange} name="subscriberEmail" type="text" placeholder="Email Address" className="bg-white text-black border border-black py-1 px-3" ></input>
                 <button className=" bg-primary py-1 px-3 " >Subscribe</button>
-            </div>
-            <h5 className="text-xs text-gray-300 text-center">Will be used in accordance with our policy</h5>
+            </form>
+            <h5 className="text-xs md:text-base text-gray-300 text-center">Will be used in accordance with our policy</h5>
             <div className="flex gap-6" >
                 <Linkedin size={20} />
                 <Facebook size={20} />
