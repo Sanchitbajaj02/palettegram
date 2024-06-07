56e4410819eb32395c6fc2546c96a113e8ccd60f
Sanchit Bajaj <sanchit02@outlook.com>
2024-06-07 23:59:35 +0530

    refactor: ui code and enhancement of 404 component

--- src/app/[others]/page.tsx
+++ src/app/[others]/page.tsx
@@ -2,22 +2,28 @@ import { ButtonLong } from "@/components/core/buttons";
 import Footer from "@/components/core/footer";
 import Navbar from "@/components/core/navbar";
 import NotFoundPic from "/public/assets/404.png";
-import './page.css';
-
+import "./page.css";
+import Image from "next/image";
 export default function Custom404() {
   return (
     <>
       <Navbar />
       <section className="max-w-screen-lg mx-auto">
-        <div className="flex flex-col items-center justify-center h-[80vh]">
-          <img src={NotFoundPic.src} alt="not found image" className="max:w-2/3 sm:w-2/3 fluid" />
-          <ButtonLong href="/" children={"Go Home"} size={"big"}/>
+        <div className="flex flex-col items-center justify-center h-[60vh] md:h-[90vh] mb-8">
+          <Image
+            src={NotFoundPic.src}
+            alt="not found image"
+            className="md:w-2/3 fluid"
+            width={1200}
+            height={1200}
+          />
+          <ButtonLong href="/" size={"big"}>
+            Go Home
+          </ButtonLong>
         </div>
       </section>
-        <br />
-        <br />
-        <br />
-      <Footer/>
+
+      <Footer />
     </>
   );
 }
